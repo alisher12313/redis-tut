c22a41e0e66f2a04c42f5312bca58af62d75b117
Alisher <alibobaladno@gmail.com>
2026-06-20 21:42:20 +0500

    Initial commit

---
+++ src/main/java/com/pm/redistut/RedisTutApplication.java
@@ -0,0 +1,13 @@
+package com.pm.redistut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisTutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pm/redistut/RedisTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pm.redistut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
